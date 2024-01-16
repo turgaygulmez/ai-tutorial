95080f64f1f24bd40b6f53dd9c7b2bf767f87a2a
Turgay Gulmez <turgaygulmez@Turgays-MacBook-Air.local>
2024-01-16 21:57:24 +0300

    fix example

--- OpenAI/example3.js
+++ OpenAI/example3.js
@@ -11,14 +11,11 @@ async function main() {
   const chatCompletion = await openai.chat.completions.create({
     messages: [
       {
-        role: "assistant",
-        content:
-          "16/01/2024 7°C, 15/01/2024 12°C, 14/01/2024 4°C, 13/01/2024 22°C",
-      },
-      {
         role: "system",
-        content:
-          "You are a helpful assistant that reports weather. You are being provided weather data together with their respective date.",
+        content: `You are a helpful assistant that reports weather. 
+          Below are being provided weather data together with their respective date.
+          16/01/2024 7°C, 15/01/2024 12°C, 14/01/2024 4°C, 13/01/2024 22°C
+          `,
       },
       { role: "user", content: "What was the weather on 16/01/2024?" },
     ],
